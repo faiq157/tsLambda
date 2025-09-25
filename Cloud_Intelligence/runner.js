ad73df09bdf433d011eb9643aba9ea2762828b6e
faiq157 <faiqa5122@gmail.com>
2025-09-25 14:21:28 +0500

    update the runner file

--- Cloud_Intelligence/runner.js
+++ Cloud_Intelligence/runner.js
@@ -101,7 +101,7 @@ const weather_snow_stow = {
   type: 'elastic_search-1',
   site_data: { id: '5dfe0df4-78a4-4522-b555-3d26798f6f54' }
 };
-let weather_stow_alarm = {
+const weather_stow_alarm = {
   "channel": "local_weather_stow_alarms",
   "timestamp": "2021-07-14T18:16:14Z",
   "start": {
@@ -148,7 +148,7 @@ let weather_stow_alarm = {
   }
 };
 
-let nc_state = {
+const nc_state = {
   asset_id: '0384195d-d0bc-403e-9272-b5bd63dbe683',
   timestamp: '2021-10-06T06:40:31.605Z',
   commanded_state: 4,
@@ -158,7 +158,7 @@ let nc_state = {
   channel: 'tracking_command_hist'
 }
 
-let snow_shedding_report = {
+const snow_shedding_report = {
   "type": "elastic_search-1",
   "channel": "snow_shedding_report",
   "network_controller_id": "da6b4184-9495-4126-a5eb-16b721a298d1",
@@ -169,7 +169,7 @@ let snow_shedding_report = {
   "timestamp": "2023-01-04T08:20:34.620Z",
   "reportDetail": "{\"snow_shedding_report.failed\":[{\"snap_address\":\"13b4aa\",\"row_id\":5}],\"snow_shedding_report.unresponsive\":[{\"snap_address\":\"13b4b8\",\"row_id\":4},{\"snap_address\":\"13b4c3\",\"row_id\":6}]}"
 }
-let snow_shedding_report_1 = {
+const snow_shedding_report_1 = {
   type: "elastic_search-1",
   channel: "snow_shedding_report",
   network_controller_id: "e177ebd3-a357-4ff7-9149-248b11bc52ec",
@@ -181,10 +181,10 @@ let snow_shedding_report_1 = {
   reportDetail: "{\"snow_shedding_report.delayed\":true,\"snow_shedding_report.completed\":[{\"snap_address\":\"0ac44f\",\"row_id\":null}],\"snow_shedding_report.retried\":[{\"snap_address\":\"0ac44f\",\"row_id\":null}],\"snow_shedding_report.retried_frequency\":[0],\"snow_shedding_report.failed\":[{\"snap_address\":\"0ac44f\",\"row_id\":null}],\"snow_shedding_report.unresponsive\":[{\"snap_address\":\"0ac44f\",\"row_id\":null}]}",
 }
 
-let snow_shedding = {
+const snow_shedding = {
   "type": "elastic_search-1", "channel": "snow_shedding", "network_controller_id": "91ce1290-a774-4bde-aa9d-4338c5ac6b49", "snap_addr": "13b51e", "asset_id": "00000000-0000-0000-0000-00000013b51e", "nc_snap_addr": "19b155", "site_id": "46897a78-f4e5-4c94-8eb1-abb88a1048e8", "timestamp": "2023-01-19T07:59:29.995Z", "snow_shedding.depth": 8.26771627730272, "snow_shedding.baseline": 0.39370077860167646, "snow_shedding.snow_on_ground_now": 0.39370077860167646, "snow_shedding.estimate": 0, "snow_shedding.trigger": 4.330708637950927, "snow_shedding.threshold": 3.937007932681737, "snow_shedding.active": false, "snow_shedding.last_snow_shedding": "1674040980", "snow_shedding.state": 2, "nc_asset_id": "481ee11b-45c3-476e-9b1f-1f1430b95b9f"
 };
-let machine_learning = {
+const machine_learning = {
   channel: 'machine_learning',
   asset_id: '00000000-0000-0000-0000-00000013b4b8',
   timestamp: '2023-03-09T07:50:47Z',
@@ -194,7 +194,7 @@ let machine_learning = {
   type: 'elastic_search-1'
 }
 
-let weather_stow = {
+const weather_stow = {
   asset_id: '432fe7dc-fae8-49f2-b1d0-16cdc84bda1d',
   asset_name: 'Inverter 1: WX 1',
   device_type_id: '14182d37-accb-4752-8558-ea874bee2e24',
@@ -207,7 +207,7 @@ let weather_stow = {
   channel: 'weather_stow_updates'
 }
 
-let weather_stow_clear = {
+const weather_stow_clear = {
   asset_id: '68e3ef6e-2e8b-42ed-b93b-5c81c0135a68', //NC asset id
   asset_name: 'Network Controller',
   device_type_id: '9c0720c1-c980-4bc4-b837-f6d5bf9c7bd4',
@@ -220,7 +220,7 @@ let weather_stow_clear = {
   channel: 'weather_stow_updates'
 }
 
-let assetConnectionHist = {
+const assetConnectionHist = {
   asset_id: '00000000-0000-0000-0000-0000001bebe7',
   connected_state: 'QC FASTTRAK',
   last_connected: '2023-07-06T05:47:27.271Z',
@@ -238,7 +238,7 @@ let assetConnectionHist = {
   status_bits: "",
 }
 
-let local_weather_nc_stow_update = {
+const local_weather_nc_stow_update = {
   "type": "elastic_search-1",
   "channel": "local_weather_nc_stow_update",
   "network_controller_id": "b607e7cd-b19c-4bc0-8b20-e1713225ac66",
@@ -277,7 +277,15 @@ var event = {
       receiptHandle:
         "AQEBPppJVgl3rXkQ6tbbLSeopsVj2CTEU+eOxyd+FjCIcNHEMabXShmnM3M8FKX/wlAiDyLISRfrZ2EDDYAC9LNvP85xsdlMOFwyKsAMq0IfUdqDVg/e6Hk9skR/jSU9fMQ+bqWlssq7u65Q9AAHY8vXmZQ/AuxjtQdUhZ2ns7CBIRYgAYtuoGnXIo9YAIPencHBr6cPVhPcFjpuuoS/T7Boveo8Mhx6Yl9wBya9sabhiMtWAm9gpqWuu7v7FXxU1jTh83kGn9kLfqk1ACluYvaPoiGiJFGyY/T6SLrEx32axU7L4cVa5bXDsKA/vRoTbtzHIAnddRbaSF+eRyHy0KZ99gTzURYXtoASKSh7dtQPStw24kG9LcKs/MbDYdV0u0QiKnHM5lNmQSezgvh9yoGWwQ==",
       Sns: {
-        Message: '{"snap_addr": "13b51c","timestamp": "2021-07-06T11:10:33.908Z","nc_id": "6bb3ced8-b9f1-455b-bc6e-aa4f9168b8ee","modeStatus": true,"temperature_threshold":50,"daysHist":[50,45,60],"type": "elastic_search-1", "channel": "vegetation_update"}'
+        Message: '{"wake_up_time":"11286","timestamp":"2025-09-25T06:44:42.320Z","network_controller_id":"da74ab50-58b3-4b80-8a1d-c2789e4efea8","asset_id":"23d41cbb-08ad-4a72-9011-9a3c1a233b6c","name":"Mars Hill NC","type":"elastic_search-1","channel":"network_controller_sleep_event","nc_snap_addr":"1be440","site_id":"fa28b5e0-ec4c-42b2-916e-ccaec5d2f3c7"}'
+        // '{"fwVersion":"2.5.1","nccbUptime":"274D 06:42:12","linuxUptime":"0D 00:00:54","applicationUptime":"0D 00:00:15","timestamp":"2025-09-24T20:36:33.120Z","network_controller_id":"79c66c4d-64f0-4999-a0e4-0168acae6c60","asset_id":"00000000-0000-0000-0000-0000001d50e0","name":"Canandaigua West NC","type":"elastic_search-1","channel":"network_controller_reboot_event","nc_snap_addr":"1d50e0","site_id":"b26e0795-7063-43d9-8adb-18ae12f7d2e1"}'
+        // '{"channel":"local_weather_nc_online","timestamp":"2025-09-25T01:18:17.350Z","network_controller_id":"2eeb9f8a-efce-4493-b457-920535cc855e","type":"elastic_search-1"}'
+        // '{"asset_id":"00000000-0000-0000-0000-000000260eb6","timestamp":"2025-09-25T01:03:07.490Z","reporting_NC":"7e30d570-ef66-450b-b75b-4bc584dbe433","registered_NC":"ab9648b6-b7e3-48f6-9d2d-c802ceb522b2","snap_addr":"260eb6","channel":"asset_wrong_reporting","type":"elastic_search-1","nc_snap_addr":"243702","site_id":"a68ccd12-1985-4ef9-bb54-1bbc883d6a46"}'
+        // '{"asset_id":"00000000-0000-0000-0000-00000026057a","device_type":"1d6e2838-8ac9-42f2-8418-778a95a27e75","timestamp":"2025-09-25T06:39:23.904Z","panel_index":16,"panel_command_state":6,"type":"elastic_search-1","channel":"asset_preset_update","nc_snap_addr":"1d51e5","site_id":"89de5d4c-2c7b-443b-9c23-2ce566367955"}'
+        // '{"timestamp":"2025-09-25T07:25:19.565Z","snapAddr":"13b4b8","stage":"FLAT","user_name":"Saad Saud","user_email":"saad.saud@zigron.com","siteMode":4,"type":"elastic_search-1","channel":"qc_update","nc_snap_addr":"19b09d","site_id":"86bf0d49-2df5-4dbe-82a1-0cafbe53ac31"}'
+        // '{"timestamp":"2025-09-25T06:48:49.865Z","snapAddr":"","reportingType":1,"reportingFlag":0,"reportingValue":0,"reportingThreshold":15,"closePercentage":75,"network_controller_id":"910a93c2-b56d-4964-a143-0b36fefdf213","type":"elastic_search-1","channel":"increase_weather_reporting_update","nc_snap_addr":"209ca2","site_id":"2bbaa107-cd60-4fc6-b07a-6683d687fa87"}'
+        // '{"type":"elastic_search-1","channel":"snow_shedding","network_controller_id":"910a93c2-b56d-4964-a143-0b36fefdf213","snap_addr":"209ca2","asset_id":"00000000-0000-0000-0000-000000209ca2","nc_snap_addr":"209ca2","site_id":"2bbaa107-cd60-4fc6-b07a-6683d687fa87","timestamp":"2025-09-25T07:22:31.690Z","nc_asset_id":"00000000-0000-0000-0000-000000209ca2","snow_shedding.depth":0,"snow_shedding.baseline":0,"snow_shedding.snow_on_ground_now":0,"snow_shedding.estimate":0,"snow_shedding.trigger":3.937007932681737,"snow_shedding.threshold":3.937007932681737,"snow_shedding.active":false,"snow_shedding.last_snow_shedding":946684800,"snow_shedding.state":1}'
+        // '{"snap_addr": "13b51c","timestamp": "2021-07-06T11:10:33.908Z","nc_id": "6bb3ced8-b9f1-455b-bc6e-aa4f9168b8ee","modeStatus": true,"temperature_threshold":50,"daysHist":[50,45,60],"type": "elastic_search-1", "channel": "vegetation_update"}'
         // '{"channel":"battery_hist","asset_id":"00000000-0000-0000-0000-0000001bebe7","snap_addr":"1bebe7","timestamp":"2023-11-16T18:53:06.408Z","current":0.019999999552965164,"voltage":13.899999618530273,"poc":20,"poh":100,"battery_temperature":66.5,"heater_temperature":68.30000305175781,"type":"elastic_search-1","channel":"battery_hist"}',
           //"{'snap_addr': '13b51c','timestamp': '2021-07-06T11:10:33.908Z','nc_id': '8ea65d78-6e75-4b22-be07-16b81e72be0b','modeStatus': true,'temperature_threshold':50,'daysHist':[50,45,60],'type': 'elastic_search-1', 'channel': 'vegetation_update'}"
           // '{"snap_addr": "13b51c","timestamp": "2021-07-06T11:10:33.908Z","nc_id": "6bb3ced8-b9f1-455b-bc6e-aa4f9168b8ee","modeStatus": true,"temperature_threshold":50,"daysHist":[50,45,60],"type": "elastic_search-1", "channel": "vegetation_update"}'
